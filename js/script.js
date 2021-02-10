c641fd5e0f1af500497c9fd7efafe9bf7ff0d595
Sergey <sergey.index@gmail.com>
2021-02-10 13:40:05 +0300

    Cirli i usloviya

--- js/script.js
+++ js/script.js
@@ -2,19 +2,30 @@ console.log('Hello');
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-const: numberOfFilms,
+count: numberOfFilms,
 movies: {},
 actors: {},
 genres: [],
 privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''), 
-    b = prompt('На сколько оцение его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''), 
-    d = prompt('На сколько оцение его?', '');
+   
 
+for(let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''), 
+          b = prompt('На сколько оцение его?', '');
+if(a !== null && a.length < 50 && b !== null && a!== '' && b !== ''){
     personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+} else {
+    i--;
+}   
+}if (personalMovieDB.count < 10){
+console.log('Это мало');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Это средний показатель');   
+} else if (personalMovieDB.count >= 30){
+    console.log('Отлично! Вы киноман!');
+}
+         
 
-    console.log(personalMovieDB);
+console.log(personalMovieDB);
