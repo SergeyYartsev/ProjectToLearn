bf2737a450fb68c14f3b3695474c13c88f545746
Sergey <sergey.index@gmail.com>
2021-02-10 10:59:28 +0300

    Sozdanie pervogo proekta

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 console.log('Hello');
-//  alert('Hello');
-const result = confirm('Ты еще тут?');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+const: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''), 
+    b = prompt('На сколько оцение его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''), 
+    d = prompt('На сколько оцение его?', '');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
