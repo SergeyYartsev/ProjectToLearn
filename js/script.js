20e7bbe1fbfdb28e7f444fc2db17ef9d1c955635
SergeyYartsev <78843331+SergeyYartsev@users.noreply.github.com>
2021-03-24 09:37:49 +0300

    go

--- js/script.js
+++ js/script.js
@@ -1,66 +1,108 @@
-console.log('Hello');
-let numberOfFilms;
-function start(){
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-    while (numberOfFilms == '' || numberOfFilms === null || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// This is a function for switch------------------------------------------------------
 
+const switchNotes = document.querySelector('.switch_notes');
+const switchLatters = document.querySelector('.switch_latters');
+let whiteButtons = document.querySelectorAll('.white_key');
+let blackButtons = document.querySelectorAll('.black_key');
+
+switchNotes.addEventListener('click', switchNote);
+switchLatters.addEventListener('click', switchLatter);
+
+
+function switchNote () {
+    
+    for(let i = 0; i < whiteButtons.length; i++) {
+        whiteButtons[i].classList.add('white_keyChange');
+        whiteButtons[i].classList.remove('white_key');
+        
+    }
+    for(let i = 0; i < blackButtons.length; i++) {
+        blackButtons[i].classList.add('black_keyChange');
+        blackButtons[i].classList.remove('black_key');
+        
     }
 }
-
-start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = prompt('На сколько оцение его?', '');
-        if (a !== null && a.length < 50 && b !== null && a !== '' && b !== '') {
-            personalMovieDB.movies[a] = b;
-        } else {
-            i--;
-        }
+function switchLatter () {
+    
+    for(let i = 0; i < whiteButtons.length; i++) {
+        whiteButtons[i].classList.add('white_key');
+        whiteButtons[i].classList.remove('white_keyChange');
+        
+    }
+    for(let i = 0; i < blackButtons.length; i++) {
+        blackButtons[i].classList.add('black_key');
+        blackButtons[i].classList.remove('black_keyChange');
+        
     }
 }
+// This is a function for sound------------------------------------------------------
+const PIANOWHITEBTN = document.querySelectorAll('.white_key');
+const PIANOBLACKBTN = document.querySelectorAll('.black_key');
+const PIANOWHITE = document.getElementById('white');
+const PIANOBLACK = document.getElementById('black');
 
-rememberMyFilms();
-
-
-function detectPersonalLevel (){
-    if (personalMovieDB.count < 10) {
-        console.log('Это мало');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('Это средний показатель');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('Отлично! Вы киноман!');
-    }
+const startSoundNow = (event) => {
+    event.target.classList.add('active');
+    const SOUND = document.getElementById(event.target.dataset.note);
+    SOUND.currentTime = 0;
+    SOUND.play();
+}
+const stopSoundNow = (event) => {
+    event.target.classList.remove('active');
 }
 
-detectPersonalLevel ();
-
-function showMyDB (hidden) {
-if (!hidden) {
-    console.log(personalMovieDB);
+const startSound = (event) => {
+    event.target.classList.add('active');
+    const SOUND = document.getElementById(event.target.dataset.note);
+    SOUND.currentTime = 0;
+    SOUND.play();
+    PIANOWHITEBTN.forEach((elem) => {
+    elem.addEventListener('mouseover', startSoundNow);
+    elem.addEventListener('mouseout', stopSoundNow);
+    })
 }
 
+const stopSound = () => {
+    PIANOWHITEBTN.forEach((elem) => {
+    elem.classList.remove('active');
+    elem.removeEventListener('mouseover', startSoundNow);
+    elem.removeEventListener('mouseout', stopSoundNow);
+    })
 }
-detectPersonalLevel ();
 
-function writeYouGenres() { 
-    for (let i = 1; i <=3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером${i}`);
-    }
+PIANOWHITE.addEventListener('mousedown', startSound, false);
+PIANOWHITE.addEventListener('mouseup', stopSound);
+
+
+const startSoundNowB = (eventb) => {
+    eventb.target.classList.add('active');
+    const SOUND = document.getElementById(eventb.target.dataset.note);
+    SOUND.currentTime = 0;
+    SOUND.play();
+}
+const stopSoundNowB = (eventb) => {
+    eventb.target.classList.remove('active');
 }
 
-writeYouGenres ();
+const startSoundB = (eventb) => {
+    eventb.target.classList.add('active');
+    const SOUND = document.getElementById(eventb.target.dataset.note);
+    SOUND.currentTime = 0;
+    SOUND.play();
+    PIANOBLACKBTN.forEach((elemb) => {
+    elemb.addEventListener('mouseover', startSoundNowB);
+    elemb.addEventListener('mouseout', stopSoundNowB);
+    })
+}
 
+const stopSoundB = () => {
+    PIANOBLACKBTN.forEach((elemb) => {
+    elemb.classList.remove('active');
+    elemb.removeEventListener('mouseover', startSoundNowB);
+    elemb.removeEventListener('mouseout', stopSoundNowB);
+    })
+}
 
-console.log(personalMovieDB);
+PIANOBLACK.addEventListener('mousedown', startSoundB, false);
+PIANOBLACK.addEventListener('mouseup', stopSoundB);
